6c4149313ec126cd4351c165776a52d04b17d68c
Onah Obinna Samuel <samgb042@gmail.com>
2022-11-20 10:33:55 +0100

    deploy

--- components/Spinner.jsx
+++ components/Spinner.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Image from 'next/image';
 import spinner from '../public/spinner.gif'
 
 const Spinner = () => {
   return (
     <>
-        <Image className="w-[200px] m-auto block" src={spinner} alt="loading.." />
+        <Image
+         className="w-[200px] m-auto block" 
+         src={spinner} 
+         alt="loading.." 
+         width='100'
+         height='100'
+        />
     </>
   )
 }
